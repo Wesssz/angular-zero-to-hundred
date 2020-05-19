2ae1d833468ef579eb2bac738268c15e55667343
Wesssz <56195621+Wesssz@users.noreply.github.com>
2020-05-19 17:10:54 +0200

    Fixed app logic, added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-zero-to-hundred';
+  randomNumber: number;
+  player1num: string;
+  player2num: string;
+  winner: string;
+
+  getRandomIntInclusive() {
+    this.randomNumber = Math.floor(Math.random() * 100) + 1;
+    return this.randomNumber;
+  }
+
+  diffCheck(a, b) {
+    return Math.abs(a - b);
+  }
+
+  isValid(a, b) {
+    if (a === b || a < 1 || a > 100 || b < 1 || b > 100) {
+      return false;
+    }
+    return true;
+  }
+
+  submitHandler() {
+    const valCheck = this.isValid(this.player1num, this.player2num);
+    if (!valCheck) {
+      return alert('Numbers cannot be equal, less than 1 or higher than 100!');
+    }
+    this.getRandomIntInclusive();
+    if (
+      this.diffCheck(this.player1num, this.randomNumber) <
+      this.diffCheck(this.player2num, this.randomNumber)
+    ) {
+      this.winner = 'Player 1';
+    } else {
+      this.winner = 'Player 2';
+    }
+  }
+
+  resetHandler() {
+    this.winner = null;
+    this.player1num = null;
+    this.player2num = null;
+  }
 }
